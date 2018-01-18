e84bf06b224f870baf7738bb992e9da3dbce9aca
Joe s kolengaden <35306176+joeskolengaden@users.noreply.github.com>
2018-01-18 06:42:50 +0000

    optimized the code
    overall optimization.
    improved codes.
    removed unwanted codes.
    added code descriptions.

--- football_twoHalfs/football_twoHalfs.ino
+++ football_twoHalfs/football_twoHalfs.ino
@@ -1,4 +1,9 @@
- /*This example illustrates two user funcions of LedP10 library
+// this is a football timmer that works with p10 non-smd LED pannels and controlled by an arduino
+
+
+// the below code is reffered from p10 library availble in github for arduino 
+
+/*This example illustrates two user funcions of LedP10 library
   *First function-showmsg_single_scroll is for scroling of single message on P10 panel with 4 arguments which are message, no. of times of message scrolling, speed and font.
   *for message, pointer to a character string can be passed. For continuously running the message, INF can be passed in second argument, spped can be in range of 0-30, 
    and font can be 0 or 1 as two type of fonts are currently supported for full screen messages. 
@@ -14,32 +19,28 @@
 #include <String.h>
 
 LedP10 myled;
-const int buttonPin = 2;
-//const int buttonPin2 = 5;
+const int buttonPin = 2;  // input pin that determines the halfs
 
 int brightness=255; // value could be from 0 to 255
 
 int mpin=0;  //input pin initial vlaue
-//int mpin2=1;
 int s=0;
 int m=0;
 void setup() 
 { 
     pinMode(buttonPin, INPUT);
-    //pinMode(buttonPin2, INPUT);
  
     myled.setbrightness(brightness);      //set brigthness
 
     myled.init(3,4,8,9 ,2);
-    myled.showmsg_single_scroll("WELCOME",1,3,0);
+    myled.showmsg_single_scroll("WELCOME",1,3,0);                  //welcome msg
+    myled.showmsg_single_scroll("    Let's FOOTBALL   ",1,3,0);   // intro msg
     delay(2000);
     
-     mpin = digitalRead(buttonPin);
-    // mpin2 = digitalRead(buttonPin2);
+     mpin = digitalRead(buttonPin);  // read the value of the input pin to variable mpin
 
-    //input to select minute to be 0 or 30
     
-    if(mpin==LOW)
+    if(mpin==LOW)    //input will set minutes variable m  to 0 or 30 based on input value
     {
      
          m=0;
@@ -55,25 +56,20 @@ void setup()
     }
 delay(3000);
     
-    //myled.showmsg_double_scroll("this is double led test1","this is double led test2",10,INF,3,1,0); 
 }
 
 void loop() {
   
-  if(s==60)
+  if(s==60) //increment variable m when variable s=60 
   {
     m=m+1;
-    s=0;
+    s=0;     // reset variable s
   }
   char msg[10];
-  sprintf(msg, " %d :: %d", m, s);
+  sprintf(msg, " %d :: %d", m, s);  // join all variable together into a string named msg
+ 
+  myled.showmsg_single_static(msg,0);// display the msg
   
-  //char timer[7] = m2 + ':' + s2;
-            
-
-  myled.showmsg_single_static(msg,0);
-  // mpin = digitalRead(buttonPin);
-  
-  s=s+1;
-  delay(880);
+  s=s+1; // increment second's variable s
+  delay(880);  // 800 milli seconds delay  for each second. The rest 200 milli seconds is taken by the p10 diplay functions
 }
